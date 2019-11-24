2bc29a3deff6c06435c8479137ca2529eebc4bdb
PinTeh <794409767@qq.com>
2019-11-24 21:43:35 +0800

    调整个人中心界面布局

--- app/src/main/java/cn/imhtb/bytemarket/ui/fragment/MineFragment.java
+++ app/src/main/java/cn/imhtb/bytemarket/ui/fragment/MineFragment.java
@@ -12,11 +12,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.imhtb.bytemarket.ui.activity.FavourActivity;
 import cn.imhtb.bytemarket.R;
 
@@ -25,14 +29,11 @@ import cn.imhtb.bytemarket.R;
  */
 public class MineFragment extends Fragment implements View.OnClickListener {
 
-    static {
-        //设置全局的Header构建器
-        SmartRefreshLayout.setDefaultRefreshHeaderCreator((context, layout) -> {
-            layout.setPrimaryColorsId(R.color.colorAccent, R.color.colorAccent);//全局设置主题颜色
-            return new ClassicsHeader(context);
-            //.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
-        });
-    }
+    @BindView(R.id.rl_mine_login)
+    RelativeLayout relativeLayout;
+
+    @BindView(R.id.ll_mine_un_login)
+    LinearLayout linearLayout;
 
     public MineFragment() {
 
@@ -48,16 +49,21 @@ public class MineFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        LinearLayout favour = view.findViewById(R.id.ll_favour);
-        LinearLayout history = view.findViewById(R.id.ll_history);
-        favour.setOnClickListener(this);
-        history.setOnClickListener(this);
-
-        SmartRefreshLayout smartRefreshLayout = view.findViewById(R.id.srl_mine_wrap);
-        smartRefreshLayout.setEnableLoadMore(false);
-        smartRefreshLayout.setOnRefreshListener(v->smartRefreshLayout.finishRefresh());
+        ButterKnife.bind(this,view);
+        initLoginComponent(false);
     }
 
+    private void initLoginComponent(boolean login) {
+        if (!login){
+            linearLayout.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+        }else{
+            linearLayout.setVisibility(View.GONE);
+            relativeLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @OnClick({R.id.ll_favour,R.id.ll_history,R.id.ll_mine_un_login,R.id.rl_mine_login})
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -70,6 +76,12 @@ public class MineFragment extends Fragment implements View.OnClickListener {
                 Intent intent = new Intent(getActivity(), FavourActivity.class);
                 intent.putExtra("desc","历史记录");
                 startActivity(intent);
+            } break;
+            case R.id.ll_mine_un_login:{
+                initLoginComponent(true);
+            } break;
+            case R.id.rl_mine_login:{
+                initLoginComponent(false);
             }
         }
     }
